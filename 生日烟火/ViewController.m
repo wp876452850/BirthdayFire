c6982528d461c38947a708c50b03c183c26054fb
rexsu <rexsu@rexsudeMac-mini.local>
2017-02-24 11:15:49 +0800

    Initial Commit

---
+++ 生日烟火/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  生日烟火
+//
+//  Created by rexsu on 2017/2/24.
+//  Copyright © 2017年 Winny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
